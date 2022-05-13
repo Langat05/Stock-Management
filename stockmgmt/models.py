01ec98e22b57d711d9ce5459183d39935d0d4248
Justo <justuslangat78@gmail.com>
2022-05-13 14:23:43 +0300

    Fisrt commit

---
+++ stockmgmt/models.py
@@ -0,0 +1,37 @@
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+
+category_choice = (
+    ('Accaracides', 'Accaracides'),
+    ('Injectibles', 'Injectibles'),
+    ('Salts', 'Salts'),
+    ('Dewormers', 'Dewormers'),
+    ('Levamisoles', 'Levamisoles'),
+    ('Seeds', 'Seeds'),
+)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Stock(models.Model):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, blank=True)
+    item_name = models.CharField(max_length=50, blank=True, null=True)
+    quantity = models.IntegerField(default='0', blank=False, null=True)
+    receive_quantity = models.IntegerField(default='0', blank=True, null=True)
+    received_from = models.CharField(max_length=30, blank=True, null=True)
+    issue_quantity = models.IntegerField(default='0', blank=True, null=True)
+    last_updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    reoder_level = models.IntegerField(default='0', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+
+def __str__(self):
+    return self.item_name
---
+++ stockmgmt/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from stockmgmt import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('list_item/', views.list_item, name='list_item'),
+    path('add_items/', views.add_items, name='add_items'),
+    path('update_items/<str:pk>/', views.update_items, name="update_items"),
+    path('delete_item/<str:pk>/', views.delete_items, name="delete_item"),
+    path('stock_detail/<str:pk>/', views.stock_detail, name="stock_detail"),
+    path('issue_items/<str:pk>/', views.issue_items, name="issue_items"),
+    path('receive_items/<str:pk>/', views.receive_items, name="receive_items"),
+]
